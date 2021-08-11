98ff54975021d4aa1c8bba4bcf2ca053d783cc85
proll4588 <prol4588@gmail.com>
2021-08-11 18:08:29 +0700

    config webpack for pug and fonst

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const miniCss = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
@@ -11,29 +12,42 @@ module.exports = {
         app: "./index.js",
     },
     output: {
-        filename: "[name].js",
+        filename: "js/[name].js",
         path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-        new miniCss({
-            filename: "style.css",
-        }),
-        new HtmlWebpackPlugin(/*{
-            filename: "index.html",
-            minify: false,
-        }*/),
-        new HtmlWebpackPugPlugin(),
         new CleanWebpackPlugin(),
+        new miniCss({
+            filename: "style/style.css",
+        }),
+        new HtmlWebpackPlugin({
+            template: "index.pug",
+            filename: "pages/index.html",
+            minify: false,
+        }),
+        new HtmlWebpackPugPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src/fonts"),
+                    to: path.resolve(__dirname, "dist/fonts"),
+                },
+            ],
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.(s*)css$/,
-                use: [miniCss.loader, "css-loader", "sass-loader"],
+                use: [
+                    { loader: miniCss.loader },
+                    { loader: "css-loader", options: { url: false } },
+                    { loader: "sass-loader" },
+                ],
             },
             {
                 test: /\.pug$/,
-                use: ["pug-loader"],
+                use: [{ loader: "pug-loader" }],
             },
         ],
     },
